0c54399fa5ea33dd1d50ec75b79b1eb8fe84b8fe
Veronchi <zhigunova1995@gmail.com>
2021-12-09 21:06:45 +0300

    implement the structure of app and add categories

--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Categories = () => {
-  return <h2>categories component</h2>;
+  return (
+    <div className='btn-container'>
+      <button type='button' className='filter-btn'>all</button>
+      <button type='button' className='filter-btn'>breakfast</button>
+      <button type='button' className='filter-btn'>lunch</button>
+      <button type='button' className='filter-btn'>shakes</button>
+    </div>
+  );
 };
 
 export default Categories;
